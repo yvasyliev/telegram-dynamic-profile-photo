eb5c0e456545f45c88f42e465292a9c515a76bf5
Yevhen Vasyliev <ye.vasyliev@gmail.com>
2023-11-25 21:02:18 +0200

    redirecting to Telegram bot

--- docs/index.js
+++ docs/index.js
@@ -1,4 +1,12 @@
 redirectToBot = () => {
     const queryString = window.location.search;
-    console.log(queryString);
+    const urlParams = new URLSearchParams(queryString);
+    const bot = urlParams.get('bot');
+    const code = urlParams.get('code');
+    if (bot && code) {
+        const url = new URL(bot, "https://t.me");
+        url.searchParams.append("start", code);
+        document.body.innerHTML = `<p>If you haven't been automatically redirected to Telegram bot, please follow the link: <a href="${url}">${url}</a></p>${document.body.innerHTML}`;
+        window.location.replace(url);
+    }
 }
